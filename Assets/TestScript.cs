f16af406a9b5373d056636197f76c0fcd6aa5e5c
hoff26 <hoff26@live.dk>
2016-04-21 10:18:57 +0200

    Testscript

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class TestScript : MonoBehaviour {
+public class TestScript : MonoBehaviour
+{
+    public string txt;
+    public int hej;
+    private float hej1;
+    public float x;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int ReturnInt()
+    {
+        Debug.Log(txt);
+        return hej;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        Debug.Log(ReturnInt());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float variabel = 4 / x;
+        if (Time.timeSinceLevelLoad > 5)
+        {
+            if (variabel == 20)
+            {
+                Debug.Log("Din variabel er 20");
+            }
+            else
+            {
+                Debug.Log("Din variabel er " + variabel);
+            }
+        }
+    }
 }
